9e5a5c9fce639863f87798f94da6c47928d5e150
Nick Keenan <nkeenan@seas.upenn.edu>
2018-11-30 14:48:09 -0500

    server changes

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 '''server/app.py - main api app declaration'''
 from flask import Flask, jsonify, send_from_directory
 from flask_cors import CORS
+import csv
 
 '''Main wrapper for app creation'''
 app = Flask(__name__, static_folder='../build')
@@ -10,10 +11,32 @@ CORS(app)
 # API routes
 ##
 
-@app.route('/api/items')
-def items():
-  '''Sample API route for data'''
-  return jsonify([{'title': 'A'}, {'title': 'B'}])
+@app.route('/api/search/mesh/<term>')
+def search_mesh(term):
+    '''Route to search for publications by MeSH Term(s)'''
+    publication_id = -1
+    with open('record_results/medical_record.csv') as mesh_csv:
+        csv_reader = csv.reader(mesh_csv, delimeter=',')
+        for row in csv_reader:
+            # if query matches MeSH term or ID
+            if row[1] == term || row[2] == terms:
+                publication_id = row[0]
+                break
+    with open('record_results/paper_record.csv') as pub_csv:
+        csv_reader = csv.reader(pub_csv, delimeter=',')
+        for row in csv_reader:
+            if row[0] == publication_id:
+                return jsonify([
+                    {'id': row[0]},
+                    {'title': row[1]},
+                    {'abstract': row[2]},
+                    {'year': row[3]},
+                    {'month': row[4]},
+                    {'author_list': row[5]},
+                    {'subject_list': row[6]},
+                    {'date': row[7]}]);
+    # if miss, return empty list
+    return jsonify([]);
 
 ##
 # View route
