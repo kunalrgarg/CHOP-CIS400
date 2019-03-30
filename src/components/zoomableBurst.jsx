ff2f7ca634b34c0714454afa6458d14352dda7c7
Kunal <kunalrgarg@gmail.com>
2019-03-30 16:10:16 -0400

    added base for zoomableBurst

---
+++ src/components/zoomableBurst.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ZoomableBurst extends Component {
+
+    
+    render() {
+        return (
+            <h1>zoomableBurst Testing</h1>
+        );
+    }
+}
+
+export default ZoomableBurst;
